8145f6095db78af468c99a3905c61d4ff946dbca
Kateryna Maistrenko <silchencko@gmail.com>
2020-01-19 21:25:55 +0200

    Lesson 1: print pairs of numbers if their sum is = 10

---
+++ pairs.py
@@ -0,0 +1,17 @@
+def find_pair(number, numbers):
+    for pair in numbers:
+        if int(number) + int(pair) == 10:
+             print(number + ' + ' + pair)
+
+def main(parameters):
+    index = 0
+    for number in parameters:
+        if index < len(parameters) - 1:
+            find_pair(number, parameters[index + 1:])
+        index+=1
+
+
+# main block
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
