5e08cedf09e85d4df6d048ed7588b8cf5bd28cbd
Kateryna Maistrenko <silchencko@gmail.com>
2020-01-20 10:39:05 +0200

    fix flake8

--- pairs.py
+++ pairs.py
@@ -1,14 +1,15 @@
 def find_pair(number, numbers):
     for pair in numbers:
         if int(number) + int(pair) == 10:
-             print(number + ' + ' + pair)
+            print(number + ' + ' + pair)
+
 
 def main(parameters):
     index = 0
     for number in parameters:
         if index < len(parameters) - 1:
             find_pair(number, parameters[index + 1:])
-        index+=1
+        index += 1
 
 
 # main block
