98f32dfae2d6b4b2ec50319f6058429d3c2a1f2d
Kateryna Maistrenko <silchencko@gmail.com>
2020-02-18 11:17:41 +0200

    Lesson5

---
+++ lesson5/questionnaire.py
@@ -0,0 +1,51 @@
+import datetime
+import os
+import logging
+
+
+# Record logs into 'logger.log' file
+logging.basicConfig(filename='logger.log', filemode='w', level=logging.INFO)
+
+
+# Context manager
+# logs file execution time
+class TemporaryFile:
+    def __init__(self, name, mode):
+        self.file = open(name, mode)
+
+    def __enter__(self):
+        self.start_time = datetime.datetime.now()
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.execution_time = datetime.datetime.now() - self.start_time
+        self.file.close()
+        logging.info(f'Execution time: %s  {self.execution_time}', self.file)
+
+
+def run_quiz_section(file_name, answer_file):
+    with TemporaryFile(file_name, 'r') as quiz_section:
+        lines = quiz_section.readlines()
+        for line in lines:
+            question = line.strip()
+            answer = input(f'{question} - ')
+            answer_file.write(f'{question}: {answer}\n')
+
+
+def run_quiz(path, answers_storage):
+    sections = os.listdir(path)
+    for section in sections:
+        file_name = path + '/' + section
+        run_quiz_section(file_name, answers_storage)
+
+
+def main():
+    quiz_path = 'quiz'
+    with TemporaryFile('answers.txt', 'w') as answers:
+        if os.path.exists(quiz_path):
+            run_quiz(quiz_path, answers)
+
+
+# main block
+if __name__ == '__main__':
+    main()
